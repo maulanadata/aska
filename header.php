6f34a748c6c768749a353ca4a3bc82030c933a8c
maulanadata <crypton1001@gmail.com>
2022-07-12 01:27:13 +0700

    first commit untuk push

---
+++ header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Home</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="./assets/bootstrap-5.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/bootstrap-5.2-dist/js/bootstrap.min.js">
+    </head>
+<body>
---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg bg-light ms-4">
+  <div class="container-fluid">
+      <img src="./assets/img/logo.png" alt="" width="130" class="me-4">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
