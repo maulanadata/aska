116395ffd506e9a7eed803a915c13c84b4f68e4a
maulanadata <crypton1001@gmail.com>
2022-07-14 01:46:04 +0700

    kerangka final, isotop belum berfungsi

---
+++ jumbotron.php
@@ -0,0 +1,53 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" style="background-color: black" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" style="background-color: black" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" style="background-color: black" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" style="background-color: black" aria-label="Slide 4"></button>
+  </div>
+
+  <div class="carousel-inner" style="height: 300px">
+    <div class="carousel-item active">
+      <img src="./assets/img/jumbotron.gif" height="300px" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block" style="color:black">
+        <h2 style="margin-bottom: 120px"> Internet </h2>
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/jumbotron.gif" height="300px" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block" style="color:black">
+        <h2 style="margin-bottom: 120px"> Beauty </h2>
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/jumbotron.gif" height="300px" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block" style="color:black">
+        <h2 style="margin-bottom: 120px"> Games </h2>
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/img/jumbotron.gif" height="300px" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block" style="color:black">
+        <h2 style="margin-bottom: 120px"> Photography </h2>
+        <h5>Fourth slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<br><br>
--- navbar.php
+++ navbar.php
@@ -1,10 +1,24 @@
-<nav class="navbar navbar-expand-lg bg-light ms-4">
-  <div class="container-fluid">
-      <img src="./assets/img/logo.png" alt="" width="130" class="me-4">
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid ms-4">
+      <a href="index.php">
+        <img src="./assets/img/logo.png" alt="" width="130" class="me-4">
+      </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Internet</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Beauty</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Games</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Photography</a>
         </li>
         
       </ul>
